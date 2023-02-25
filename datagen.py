39edc629c9a2c078e58e00573627abdd5063ef5f
debg48 <debgandhar4000@gmail.com>
2023-02-25 12:26:40 +0530

    range can be custom

--- datagen.py
+++ datagen.py
@@ -21,23 +21,27 @@ else:
     if route.strip()=='':
         route = 'localhost'
     db_name=input("Enter the name db\n")
-    engine_params=engine_params+db_client+"://"+db_user+":"+db_passwd+"@"+route+"/"+db_name
-    for _ in range(1000):
-        # READ DOCUMENTATION TO CUSTOMIZE FIELDS OF THE TABLE ACCORDINGLY
-        # THIS IS WHERE THE CUSTOMIATION NEEDS TO BE DONE
-        fake_data["first_name"].append( fake.first_name() )
-        fake_data["last_name"].append( fake.last_name() )
-        fake_data["occupation"].append( fake.job() )
-        fake_data["dob"].append( fake.date_of_birth() )
-        fake_data["country"].append( fake.country() )
-        fake_data["salary"].append(fake.random_int())
+    data_size=input("Enter the number of row of the table you want to create \n")
+    if not data_size.isnumeric():
+        print("Invalid Number entered !")
+    else :     
+        engine_params=engine_params+db_client+"://"+db_user+":"+db_passwd+"@"+route+"/"+db_name
+        for _ in range(data_size):
+            # READ DOCUMENTATION TO CUSTOMIZE FIELDS OF THE TABLE ACCORDINGLY
+            # THIS IS WHERE THE CUSTOMIATION NEEDS TO BE DONE
+            fake_data["first_name"].append( fake.first_name() )
+            fake_data["last_name"].append( fake.last_name() )
+            fake_data["occupation"].append( fake.job() )
+            fake_data["dob"].append( fake.date_of_birth() )
+            fake_data["country"].append( fake.country() )
+            fake_data["salary"].append(fake.random_int())
 
-    # CREATING A PANDAS DATAFRAME TO STORE THE DATA
-    df_fake_data = pd.DataFrame(fake_data)
+        # CREATING A PANDAS DATAFRAME TO STORE THE DATA
+        df_fake_data = pd.DataFrame(fake_data)
 
-    #CONNECTING TO THE SQL SERVER AND CREATING THE TABLE AND FILLING IT WITH DUMMY DATA
-    engine = create_engine(engine_params, echo=False)
-    try:
-        df_fake_data.to_sql('employee', con=engine,index=False)
-    except Exception as e:
-        print(e)
+        #CONNECTING TO THE SQL SERVER AND CREATING THE TABLE AND FILLING IT WITH DUMMY DATA
+        engine = create_engine(engine_params, echo=False)
+        try:
+            df_fake_data.to_sql('employee', con=engine,index=False)
+        except Exception as e:
+            print(e)
